db7428301b32234a76295c972d0ef5bbf1314966
vengefulbliss <zapyaplee@gmail.com>
2018-05-29 11:48:43 -0600

    liri.js finished

--- liri.js
+++ liri.js
@@ -58,3 +58,68 @@ spotify.search({ type: 'track', query: trackQuery }, function(error, data) {
 		console.log("Album: " + data.tracks.items[0].album.name);
 	}
 });
+
+var movieThis = function(movieQuery) {
+	//request npm module
+	var request = require('request');
+	//if nothing is inputted for movie this. default to "mr. nobody.
+	if(movieQuery === undefined) {
+		movieQuery = "mr nobody";
+	}
+	// GET request for http.
+	request("http://www.omdapi/?t=" + movieQuery + "&y=&plot=short&r=json", function(error, response, body) {
+		if(!error && response.statusCode === 200){
+			console.log("* Title of movie:  " + JSON.parse(body).Title);
+			console.log("*Year the movie came out: " + JSON.parse(body).Year);
+			console.log("* IMDB Rating of the movie: " + JSON.parse(body).imdbRating);
+		console.log("* Country produced: " + JSON.parse(body).Country);
+		console.log("* Language of the movie" + JSON.parse(body).Language);
+		console.log("* Plot of the movie:  " + JSON.parse(body).Plot);
+		console.log("* Actors in the movie: " + JSON.parse(body).Actors);
+		//for loop parses ratings object to see possible rotten tomato rating.
+		//console log if there is one.
+		for(var i = 0; i < JSON.parse(body).Ratings.length; i++) {
+			if(JSON.parse(body).Ratings[i].Website !== undefined) {
+				console.log("*Rotten Tomatoes URL: " + JSON.parse(body).Ratings[i].Website);
+			}
+		}
+	}
+});
+}
+// user input 
+if(command === "my-tweets") {
+	myTweets();
+} else if(command === "spofity-this-song") {
+	spotifyThisSong(query);
+} else if(command === "movie-this") {
+	movieThis(query);
+} else if(command === "do-what-it-says") {
+	//some functionality from read/load fs npm package.
+	var fs = require("fs");
+	fs.readFile("random.txt", "utf-8", function(error, data) {
+		var command;
+		var query;
+		// comma will help split the string from the file to help specify command and query. (no comma means command is "my-tweets")
+		if(data.indexOf(",") !== -1) {
+			var dataArr = data.split(",");
+			command = dataArr[0];
+			query = dataArr[1];
+		} else {
+			command = data;
+		}
+		//app functions
+		if(command === "my-tweets") {
+			myTweets();
+		} else if(command === "spotify-this-song") {
+			spotifyThisSong(query);
+		} else if(command === "movie-this") {
+			movieThis(query);
+		} else {
+			console.log("Command not valid. Please try again.")
+		}
+	});
+} else if(command === undefined) {
+	console.log("Please enter a command to run Liri")
+} else {
+	console.log("Command not valid. Please try again.")
+}
